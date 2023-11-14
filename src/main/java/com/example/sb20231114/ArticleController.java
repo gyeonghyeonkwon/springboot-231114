6f79809ea64e82328d1fac8b78067c8f9832109b
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-15 00:14:56 +0900

    article/write , step 4

--- src/main/java/com/example/sb20231114/ArticleController.java
+++ src/main/java/com/example/sb20231114/ArticleController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class ArticleController {
 
@@ -17,11 +20,13 @@ public class ArticleController {
 
     @GetMapping("/article/doWrite")
     @ResponseBody
-    Article doWrite(String title ,String body){
-
+    Map<String, Object> doWrite(String title ,String body){
         Article article = new Article(1, title , body);
 
-        return article;
+        Map<String, Object> rs = new HashMap<>();
+        rs.put ("msg " , "1번게시물이 작성되었습니다.");
+        rs.put ("data", article);
+        return rs;
     }
 }
 
